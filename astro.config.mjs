4b9ec34a0b9ff9c3780ba87fab744d7e9d7cb20b
Red <bradley.t.sheehan@gmail.com>
2025-01-20 16:08:48 -0700

    packages and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,28 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import partytown from "@astrojs/partytown";
+import react from "@astrojs/react";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [partytown({
-    config: {
-      forward: ["dataLayer.push"],
-    },
-  })],
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    react(),
+  ],
   site: "https://denver.space",
   output: "server",
   adapter: node({
-    mode: "standalone"
+    mode: "standalone",
   }),
-  integrations: [tailwind(), sitemap(), icon()]
+  integrations: [tailwind(), sitemap(), icon()],
 });
